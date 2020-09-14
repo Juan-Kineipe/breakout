17fd53689198a49fa17852333c7c310950620873
Juan Kineipe <juan.kineipe@gmail.com>
2020-09-14 15:57:06 -0300

    Add ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Respawn()
+    {
+        
+    }
+}
